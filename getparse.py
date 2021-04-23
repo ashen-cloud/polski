84905bcc5742c14335382a62512d97f8d12386d9
Dan <serendipitypwnz@gmail.com>
2021-04-24 00:11:03 +0300

    parser almost ready

--- getparse.py
+++ getparse.py
@@ -3,24 +3,11 @@
 import re
 import requests
 import string
+import sys
 
 from bs4 import BeautifulSoup
 
 url_base = "https://lingust.ru/polski/lekcje-polskiego/"
-
-lessons = [
-    "lekcja16"
-]
-
-r = requests.get(url_base + lessons[0])
-
-soup = BeautifulSoup(r.text, 'html.parser')
-
-regex = re.compile('.*col-xs-6.*')
-
-words = soup.find_all("div", {'class': regex})
-
-pol,rus = [],[]
 
 def remove_trash_before(st):
     if not len(st): return ""
@@ -37,27 +24,38 @@ def remove_trash_before(st):
         ret = ret[2:]
     return ret    
 
-for mixed_word in words:
-    clean_re = re.compile('[,\.!?<>]') 
-    for word in mixed_word.get_text().split("\n"):
-        # cleaned_word = word.replace("\n", "").replace("\r", "").replace("\t", "").replace("-\xa0", "")
-        cleaned_word = remove_trash_before(word).replace("\r", "")
-        if cleaned_word and len(cleaned_word):
-            # TODO: check for - properly
-            if cleaned_word[0].lower() in string.ascii_lowercase:
-                pol.append(cleaned_word) 
-            else:
-                rus.append(cleaned_word)
+def parse_page(l_num):
+    r = requests.get(url_base + f"lekcja{l_num}")
+    
+    soup = BeautifulSoup(r.text, 'html.parser')
+    
+    regex = re.compile('.*col-xs-6.*')
+    
+    words = soup.find_all("div", {'class': regex})
+    
+    pol,rus = [],[]
+    
+    for mixed_word in words:
+        clean_re = re.compile('[,\.!?<>]') 
+        for word in mixed_word.get_text().split("\n"):
+            cleaned_word = remove_trash_before(word).replace("\r", "")
+            if cleaned_word and len(cleaned_word):
+                if cleaned_word[0].lower() in string.ascii_lowercase:
+                    pol.append(cleaned_word) 
+                else:
+                    rus.append(cleaned_word)
+    return zip(pol, rus)
 
-dict = {}
-for p,r in zip(pol, rus):
-    dict[p] = r
+def get_all_parsed(c_arg):
+    lesson_number = 0
+    if len(c_arg) >= 2:
+        lesson_number = int(c_arg[1])
+    
+    prt = lambda ln: [print(p,r) for p,r in parse_page(ln)]
+    if lesson_number:
+        prt(lesson_number)
+    else:
+        for i in range(1, 28):
+            prt(i)
 
-print(dict)
-
-# print(soup.find_all("div", {'class': regex}))
-# [print(c.get_text()) for c in words]
-# print(r.text)
-# print(soup.get_text())
-
-
+get_all_parsed(sys.argv)
